f029816461fb19e0fa343efb63ea3a779d2f5aec
xushiwei <x@goplus.org>
2024-01-06 15:02:42 +0800

    rm check(err)

--- cmd/gopcomm/gop_autogen.go
+++ cmd/gopcomm/gop_autogen.go
@@ -3,28 +3,32 @@ package main
 import (
 	"embed"
 	"io/fs"
-	"log"
 	"github.com/goplus/yap"
+	"github.com/qiniu/x/errors"
 )
-//line cmd/gopcomm/community.gop:9:1
-func check(err error) {
-//line cmd/gopcomm/community.gop:10:1
-	if err != nil {
-//line cmd/gopcomm/community.gop:11:1
-		log.Panicln(err)
-	}
-}
 
 var yapFS embed.FS
-//line cmd/gopcomm/community.gop:18
+//line cmd/gopcomm/community.gop:11
 func main() {
-//line cmd/gopcomm/community.gop:18:1
-	fsYap, err := fs.Sub(yapFS, "yap")
-//line cmd/gopcomm/community.gop:19:1
-	check(err)
-//line cmd/gopcomm/community.gop:21:1
+//line cmd/gopcomm/community.gop:11:1
+	fsYap := func() (_gop_ret fs.FS) {
+//line cmd/gopcomm/community.gop:11:1
+		var _gop_err error
+//line cmd/gopcomm/community.gop:11:1
+		_gop_ret, _gop_err = fs.Sub(yapFS, "yap")
+//line cmd/gopcomm/community.gop:11:1
+		if _gop_err != nil {
+//line cmd/gopcomm/community.gop:11:1
+			_gop_err = errors.NewFrame(_gop_err, "fs.sub(yapFS, \"yap\")", "/Users/xushiwei/work/community/cmd/gopcomm/community.gop", 11, "main.main")
+//line cmd/gopcomm/community.gop:11:1
+			panic(_gop_err)
+		}
+//line cmd/gopcomm/community.gop:11:1
+		return
+	}()
+//line cmd/gopcomm/community.gop:12:1
 	y := yap.New(fsYap)
-//line cmd/gopcomm/community.gop:23:1
+//line cmd/gopcomm/community.gop:14:1
 	y.Handle("/p/", func(ctx *yap.Context) {
 	})
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/goplus/community
 
 go 1.18
 
-require github.com/goplus/yap v0.1.0
+require (
+    github.com/goplus/yap v0.1.0
+    github.com/qiniu/x v1.13.2
+)
