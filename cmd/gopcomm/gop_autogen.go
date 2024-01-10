1b4bd09ff07dbecb27bfad3eaf49106697dbcfe1
Bo <boironic@gmail.com>
2024-01-10 11:00:38 +0800

    fix: Update core structure to dom-like.

--- cmd/gopcomm/gop_autogen.go
+++ cmd/gopcomm/gop_autogen.go
@@ -9,35 +9,41 @@ import (
 type community struct {
 	yap.App
 }
-//line cmd/gopcomm/community_yap.gox:3
+// TODO: Config Init
+//
+//line cmd/gopcomm/community_yap.gox:4
 func (this *community) MainEntry() {
-//line cmd/gopcomm/community_yap.gox:3:1
-	this.Get("/article/:id", func(ctx *yap.Context) {
 //line cmd/gopcomm/community_yap.gox:4:1
-		param := ctx.Param("id")
-//line cmd/gopcomm/community_yap.gox:5:1
-		fmt.Println("Visiting article " + param)
-//line cmd/gopcomm/community_yap.gox:6:1
-		s := core.NewArticleService()
+	config := &core.Config{core.ArticleConfig{}}
 //line cmd/gopcomm/community_yap.gox:7:1
-		info, _ := s.GetArticle(param)
+	community := core.New(config)
 //line cmd/gopcomm/community_yap.gox:9:1
+	this.Get("/article/:id", func(ctx *yap.Context) {
+//line cmd/gopcomm/community_yap.gox:10:1
+		param := ctx.Param("id")
+//line cmd/gopcomm/community_yap.gox:11:1
+		fmt.Println("Visiting article " + param)
+//line cmd/gopcomm/community_yap.gox:13:1
+		h := community.GetArticleHandler()
+//line cmd/gopcomm/community_yap.gox:14:1
+		info, _ := h.GetArticle(param)
+//line cmd/gopcomm/community_yap.gox:16:1
 		ctx.Yap__1("article", map[string]string{"id": info.Id, "title": info.Title, "content": info.Content})
 	})
-//line cmd/gopcomm/community_yap.gox:15:1
+//line cmd/gopcomm/community_yap.gox:22:1
 	this.Get("/", func(ctx *yap.Context) {
-//line cmd/gopcomm/community_yap.gox:16:1
+//line cmd/gopcomm/community_yap.gox:23:1
 		ctx.Yap__1("home", map[string]interface {
 		}{})
 	})
-//line cmd/gopcomm/community_yap.gox:19:1
+//line cmd/gopcomm/community_yap.gox:26:1
 	this.Get("/user/:id", func(ctx *yap.Context) {
-//line cmd/gopcomm/community_yap.gox:20:1
+//line cmd/gopcomm/community_yap.gox:27:1
 		ctx.Yap__1("user", map[string]string{"id": ctx.Param("id")})
 	})
-//line cmd/gopcomm/community_yap.gox:25:1
+//line cmd/gopcomm/community_yap.gox:32:1
 	fmt.Println("Community server running on :8080")
-//line cmd/gopcomm/community_yap.gox:26:1
+//line cmd/gopcomm/community_yap.gox:33:1
 	this.Run__1(":8080")
 }
 func main() {
