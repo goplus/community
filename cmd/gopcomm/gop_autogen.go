156e386e49addd28bd9b3d82c5186faf7a4d6720
xushiwei <x@goplus.org>
2024-01-10 15:02:45 +0800

    intro uid

--- cmd/gopcomm/gop_autogen.go
+++ cmd/gopcomm/gop_autogen.go
@@ -17,7 +17,7 @@ func (this *community) MainEntry() {
 //line cmd/gopcomm/community_yap.gox:11:1
 		id := ctx.Param("id")
 //line cmd/gopcomm/community_yap.gox:12:1
-		article, _ := this.community.Article(id)
+		article, _ := this.community.Article(nil, id)
 //line cmd/gopcomm/community_yap.gox:13:1
 		html, _ := markdown.Render(article.Content)
 //line cmd/gopcomm/community_yap.gox:14:1
@@ -26,20 +26,23 @@ func (this *community) MainEntry() {
 //line cmd/gopcomm/community_yap.gox:20:1
 	this.Get("/", func(ctx *yap.Context) {
 //line cmd/gopcomm/community_yap.gox:21:1
-		articles, _, _ := this.community.ListArticle(core.MarkBegin, 20)
+		articles, _, _ := this.community.ListArticle(nil, core.MarkBegin, 20)
 //line cmd/gopcomm/community_yap.gox:22:1
 		ctx.Yap__1("home", map[string][]*core.ArticleEntry{"Items": articles})
 	})
 //line cmd/gopcomm/community_yap.gox:26:1
 	this.Get("/edit", func(ctx *yap.Context) {
-//line cmd/gopcomm/community_yap.gox:27:1
+//line cmd/gopcomm/community_yap.gox:28:1
 		ctx.Yap__1("edit", map[string]string{"ID": ctx.Param("id")})
 	})
 //line cmd/gopcomm/community_yap.gox:32:1
+	this.Post("/commit", func(ctx *yap.Context) {
+	})
+//line cmd/gopcomm/community_yap.gox:36:1
 	config := &core.Config{}
-//line cmd/gopcomm/community_yap.gox:33:1
+//line cmd/gopcomm/community_yap.gox:37:1
 	this.community, _ = core.New(config)
-//line cmd/gopcomm/community_yap.gox:35:1
+//line cmd/gopcomm/community_yap.gox:39:1
 	this.Run__1(":8080")
 }
 func main() {
--- internal/core/gop_autogen.go
+++ internal/core/gop_autogen.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"os"
+	"context"
 	"io"
 	"time"
 )
@@ -35,41 +36,48 @@ const (
 var ErrNotExist = os.ErrNotExist
 // Article returns an article.
 //
-//line internal/core/community.gop:59:1
-func (p *Community) Article(id string) (article *Article, err error) {
 //line internal/core/community.gop:60:1
-	if id == "123" {
+func (p *Community) Article(ctx context.Context, id string) (article *Article, err error) {
 //line internal/core/community.gop:61:1
+	if id == "123" {
+//line internal/core/community.gop:62:1
 		article = &Article{ArticleEntry{ID: id, Title: "Title"}, []byte(contentSummary)}
-//line internal/core/community.gop:68:1
+//line internal/core/community.gop:69:1
 		return
 	}
-//line internal/core/community.gop:70:1
+//line internal/core/community.gop:71:1
 	return nil, ErrNotExist
+}
+// CanEditable
+//
+//line internal/core/community.gop:75:1
+func (p *Community) CanEditable(ctx context.Context, uid string, id string) (editable bool, err error) {
+//line internal/core/community.gop:76:1
+	return
 }
 // PutArticle adds new article (ID == "") or edits an existing article (ID != "").
 //
-//line internal/core/community.gop:74:1
-func (p *Community) PutArticle(article *Article) (id string, err error) {
-//line internal/core/community.gop:75:1
+//line internal/core/community.gop:80:1
+func (p *Community) PutArticle(ctx context.Context, uid string, article *Article) (id string, err error) {
+//line internal/core/community.gop:81:1
 	return
 }
 // ListArticle lists articles from an position.
 //
-//line internal/core/community.gop:84:1
-func (p *Community) ListArticle(from string, limit int) (items []*ArticleEntry, next string, err error) {
-//line internal/core/community.gop:85:1
-	if from == MarkBegin {
-//line internal/core/community.gop:86:1
-		item := &ArticleEntry{ID: "123", Title: "Title"}
 //line internal/core/community.gop:90:1
+func (p *Community) ListArticle(ctx context.Context, from string, limit int) (items []*ArticleEntry, next string, err error) {
+//line internal/core/community.gop:91:1
+	if from == MarkBegin {
+//line internal/core/community.gop:92:1
+		item := &ArticleEntry{ID: "123", Title: "Title"}
+//line internal/core/community.gop:96:1
 		return []*ArticleEntry{item}, MarkEnd, nil
 	}
-//line internal/core/community.gop:92:1
+//line internal/core/community.gop:98:1
 	return nil, MarkEnd, io.EOF
 }
-//line internal/core/community.gop:47:1
-func New(conf *Config) (*Community, error) {
 //line internal/core/community.gop:48:1
+func New(conf *Config) (*Community, error) {
+//line internal/core/community.gop:49:1
 	return &Community{}, nil
 }
