9241364cbd84e13d3d4524662230a04f422ab14d
Baihhh <2542274498@qq.com>
2024-01-11 09:58:22 +0800

    feat: connect mysql and get article by id

--- cmd/gopcomm/gop_autogen.go
+++ cmd/gopcomm/gop_autogen.go
@@ -1,48 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"github.com/goplus/yap"
 	"github.com/goplus/community/internal/core"
-	"github.com/goplus/community/markdown"
+	"github.com/spf13/viper"
 )
 
 type community struct {
 	yap.App
 	community *core.Community
 }
-//line cmd/gopcomm/community_yap.gox:10
+//line cmd/gopcomm/community_yap.gox:11
 func (this *community) MainEntry() {
-//line cmd/gopcomm/community_yap.gox:10:1
-	this.Get("/p/:id", func(ctx *yap.Context) {
 //line cmd/gopcomm/community_yap.gox:11:1
-		id := ctx.Param("id")
+	this.Get("/p/:id", func(ctx *yap.Context) {
 //line cmd/gopcomm/community_yap.gox:12:1
-		article, _ := this.community.Article(nil, id)
+		id := ctx.Param("id")
 //line cmd/gopcomm/community_yap.gox:13:1
-		html, _ := markdown.Render(article.Content)
-//line cmd/gopcomm/community_yap.gox:14:1
-		ctx.Yap__1("article", map[string]string{"ID": id, "Title": article.Title, "Body": string(html)})
+		article, _ := this.community.Article(nil, id)
+//line cmd/gopcomm/community_yap.gox:15:1
+		ctx.Yap__1("article", map[string]string{"ID": id, "Title": article.Title, "Body": article.HtmlText})
 	})
-//line cmd/gopcomm/community_yap.gox:20:1
-	this.Get("/", func(ctx *yap.Context) {
 //line cmd/gopcomm/community_yap.gox:21:1
-		articles, _, _ := this.community.ListArticle(nil, core.MarkBegin, 20)
+	this.Get("/", func(ctx *yap.Context) {
 //line cmd/gopcomm/community_yap.gox:22:1
+		articles, _, _ := this.community.ListArticle(nil, core.MarkBegin, 20)
+//line cmd/gopcomm/community_yap.gox:23:1
 		ctx.Yap__1("home", map[string][]*core.ArticleEntry{"Items": articles})
 	})
-//line cmd/gopcomm/community_yap.gox:26:1
+//line cmd/gopcomm/community_yap.gox:27:1
 	this.Get("/edit", func(ctx *yap.Context) {
-//line cmd/gopcomm/community_yap.gox:28:1
+//line cmd/gopcomm/community_yap.gox:29:1
 		ctx.Yap__1("edit", map[string]string{"ID": ctx.Param("id")})
 	})
-//line cmd/gopcomm/community_yap.gox:32:1
+//line cmd/gopcomm/community_yap.gox:33:1
 	this.Post("/commit", func(ctx *yap.Context) {
 	})
-//line cmd/gopcomm/community_yap.gox:36:1
-	config := &core.Config{}
 //line cmd/gopcomm/community_yap.gox:37:1
+	viper.SetConfigFile("./config.yaml")
+//line cmd/gopcomm/community_yap.gox:38:1
+	viper.AddConfigPath(".")
+//line cmd/gopcomm/community_yap.gox:40:1
+	if
+//line cmd/gopcomm/community_yap.gox:40:1
+	err := viper.ReadInConfig(); err != nil {
+//line cmd/gopcomm/community_yap.gox:41:1
+		fmt.Println(err)
+//line cmd/gopcomm/community_yap.gox:42:1
+		return
+	}
+//line cmd/gopcomm/community_yap.gox:45:1
+	config := &core.Config{Mysql: core.Mysql{Ip: viper.GetString("mysql.ip"), Port: viper.GetInt("mysql.port"), Username: viper.GetString("mysql.username"), Password: viper.GetString("mysql.password"), Database: viper.GetString("mysql.database")}}
+//line cmd/gopcomm/community_yap.gox:56:1
 	this.community, _ = core.New(config)
-//line cmd/gopcomm/community_yap.gox:39:1
+//line cmd/gopcomm/community_yap.gox:57:1
+	core.InitMysqlDB(config)
+//line cmd/gopcomm/community_yap.gox:59:1
+	fmt.Println("start")
+//line cmd/gopcomm/community_yap.gox:60:1
 	this.Run__1(":8080")
 }
 func main() {
--- internal/core/gop_autogen.go
+++ internal/core/gop_autogen.go
@@ -1,13 +1,24 @@
 package core
 
 import (
+	"fmt"
 	"os"
 	"context"
 	"io"
 	"time"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
-
+// Mysql config.
+type Mysql struct {
+	Ip       string
+	Port     int
+	Username string
+	Password string
+	Database string
+}
 type Config struct {
+	Mysql
 }
 type ArticleEntry struct {
 	ID    string
@@ -17,7 +28,12 @@ type ArticleEntry struct {
 }
 type Article struct {
 	ArticleEntry
-	Content []byte
+	AccountId    string
+	Tags         string
+	Status       int
+	CoverId      int
+	MarkdownText string
+	HtmlText     string
 }
 type Community struct {
 }
@@ -34,71 +50,112 @@ const (
 )
 
 var ErrNotExist = os.ErrNotExist
+var DB *sql.DB
 // Article returns an article.
 //
-//line internal/core/community.gop:60:1
+//line internal/core/community.gop:127:1
 func (p *Community) Article(ctx context.Context, id string) (article *Article, err error) {
-//line internal/core/community.gop:61:1
-	if id == "123" {
-//line internal/core/community.gop:62:1
-		article = &Article{ArticleEntry{ID: id, Title: "Title"}, []byte(contentSummary)}
-//line internal/core/community.gop:69:1
-		return
+//line internal/core/community.gop:128:1
+	article = &Article{}
+//line internal/core/community.gop:129:1
+	sqlStr := "select id,title,account_id,status,markdown_text,html_text from article where id=?"
+//line internal/core/community.gop:130:1
+	err = DB.QueryRow(sqlStr, id).Scan(&article.ID, &article.Title, &article.AccountId, &article.Status, &article.MarkdownText, &article.HtmlText)
+//line internal/core/community.gop:131:1
+	if err != nil {
+//line internal/core/community.gop:132:1
+		fmt.Println("not found the article")
+//line internal/core/community.gop:133:1
+		return article, ErrNotExist
 	}
-//line internal/core/community.gop:71:1
-	return nil, ErrNotExist
+//line internal/core/community.gop:135:1
+	return
 }
 // CanEditable
 //
-//line internal/core/community.gop:75:1
+//line internal/core/community.gop:139:1
 func (p *Community) CanEditable(ctx context.Context, uid string, id string) (editable bool, err error) {
-//line internal/core/community.gop:76:1
+//line internal/core/community.gop:140:1
 	return
 }
 // PutArticle adds new article (ID == "") or edits an existing article (ID != "").
 //
-//line internal/core/community.gop:80:1
+//line internal/core/community.gop:144:1
 func (p *Community) PutArticle(ctx context.Context, uid string, article *Article) (id string, err error) {
-//line internal/core/community.gop:81:1
+//line internal/core/community.gop:145:1
 	return
 }
-//line internal/core/community.gop:84:1
+//line internal/core/community.gop:148:1
 func (p *Community) DeleteArticle(ctx context.Context, uid string, id string) (err error) {
-//line internal/core/community.gop:85:1
+//line internal/core/community.gop:149:1
 	return
 }
 // ListArticle lists articles from an position.
 //
-//line internal/core/community.gop:94:1
+//line internal/core/community.gop:158:1
 func (p *Community) ListArticle(ctx context.Context, from string, limit int) (items []*ArticleEntry, next string, err error) {
-//line internal/core/community.gop:95:1
+//line internal/core/community.gop:159:1
 	if from == MarkBegin {
-//line internal/core/community.gop:96:1
+//line internal/core/community.gop:160:1
 		item := &ArticleEntry{ID: "123", Title: "Title"}
-//line internal/core/community.gop:100:1
+//line internal/core/community.gop:164:1
 		return []*ArticleEntry{item}, MarkEnd, nil
 	}
-//line internal/core/community.gop:102:1
+//line internal/core/community.gop:166:1
 	return nil, MarkEnd, io.EOF
 }
 // PutMedia uploads media.
 //
 //line internal/core/media.gop:6:1
 func (p *Community) PutMedia(ctx context.Context, uid string, media []byte) (id string, err error) {
+//line internal/core/media.gop:7:1
 	return
 }
-//line internal/core/media.gop:9:1
-func (p *Community) DeleteMedia(ctx context.Context, uid string, id string) (err error) {
 //line internal/core/media.gop:10:1
+func (p *Community) DeleteMedia(ctx context.Context, uid string, id string) (err error) {
+//line internal/core/media.gop:11:1
 	return
 }
-//line internal/core/media.gop:13:1
-func (p *Community) MediaURL(id string) (url string) {
 //line internal/core/media.gop:14:1
+func (p *Community) MediaURL(id string) (url string) {
+//line internal/core/media.gop:15:1
 	return
 }
-//line internal/core/community.gop:48:1
+//line internal/core/community.gop:69:1
 func New(conf *Config) (*Community, error) {
-//line internal/core/community.gop:49:1
+//line internal/core/community.gop:70:1
 	return &Community{}, nil
+}
+// InitMysqlDB init mysql connection.
+//
+//line internal/core/community.gop:74:1
+func InitMysqlDB(conf *Config) {
+//line internal/core/community.gop:75:1
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", conf.Mysql.Username, conf.Mysql.Password, conf.Mysql.Ip, conf.Mysql.Port, conf.Mysql.Database)
+//line internal/core/community.gop:83:1
+	db, err := sql.Open("mysql", dsn)
+//line internal/core/community.gop:84:1
+	if err != nil {
+//line internal/core/community.gop:85:1
+		fmt.Println(err)
+//line internal/core/community.gop:86:1
+		return
+	}
+//line internal/core/community.gop:89:1
+	err = db.Ping()
+//line internal/core/community.gop:90:1
+	if err != nil {
+//line internal/core/community.gop:91:1
+		fmt.Println(err)
+//line internal/core/community.gop:92:1
+		return
+	}
+//line internal/core/community.gop:95:1
+	db.SetMaxOpenConns(10)
+//line internal/core/community.gop:96:1
+	db.SetMaxIdleConns(10)
+//line internal/core/community.gop:98:1
+	DB = db
+//line internal/core/community.gop:100:1
+	fmt.Println("mysql connected")
 }
