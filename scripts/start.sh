3ca283a84d0010dcb1018d76424ac9863c165e17
linjie <lj7674@126.com>
2024-01-22 11:35:52 +0800

    delete the unnecessary print

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,5 @@
-# run account
-cd cmd/gopcomm && cp /config/.community-env .env && go mod tidy && gop run . &
-echo "run community"
-
 # run commnuity
+cd cmd/gopcomm && cp /config/.community-env .env && go mod tidy && gop run . &
+
+# run account
 cd ../account/cmd && cp /config/.account-env .env && go mod tidy && gop run .
-echo "run account"
