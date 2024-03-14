5a9af7205217a8f4e6f3c239a850db24109369ce
LiusCraft <liuscraft@qq.com>
2024-03-14 14:16:47 +0800

    docs: update databse init sql

--- docs/sql/database-init.sql
+++ docs/sql/database-init.sql
@@ -1,62 +1,171 @@
-create table if not exists article
-(
-    id            int unsigned auto_increment
-        primary key,
-    title         varchar(255) default '' null,
-    user_id       varchar(255) default '' not null,
-    tags          varchar(255) default '' null,
-    cover         varchar(255) default '' null,
-    content       longtext                null,
-    trans_content longtext                null,
-    html_id       int                     null,
-    trans_html_id int                     null,
-    ctime         datetime                null,
-    mtime         datetime                null on update CURRENT_TIMESTAMP,
-    abstract      varchar(255) default '' null
-);
+-- MySQL dump 10.13  Distrib 8.2.0, for macos12.6 (arm64)
+--
+-- Host: mysql-0.mysql-249y.community.svc.jfcs-qa1.local    Database: community
+-- ------------------------------------------------------
+-- Server version	8.2.0
 
-create table if not exists file
-(
-    id        int auto_increment comment 'Primary Key'
-        primary key,
-    create_at datetime     null comment 'Create Time',
-    update_at datetime     null comment 'Update Time',
-    file_key  varchar(255) null,
-    format    varchar(255) null,
-    user_id   varchar(255) null,
-    size      bigint       null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table if not exists share
-(
-    id        int auto_increment
-        primary key,
-    platform  varchar(20)  not null,
-    user_id   int          null,
-    ip        varchar(50)  null,
-    index_key varchar(100) null,
-    create_at datetime     null,
-    constraint share_pk
-        unique (index_key)
-);
+--
+-- Table structure for table `article`
+--
 
-create table if not exists video_subtitle
-(
-    video_id    int         not null,
-    subtitle_id int         null,
-    user_id     int         not null,
-    language    varchar(20) not null
-);
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '',
+  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `tags` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '',
+  `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '',
+  `content` longtext,
+  `trans_content` longtext,
+  `html_id` int DEFAULT NULL,
+  `trans_html_id` int DEFAULT NULL,
+  `ctime` datetime DEFAULT NULL,
+  `mtime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `abstract` varchar(511) DEFAULT NULL,
+  `trans` tinyint(1) DEFAULT '0',
+  `trans_tags` varchar(255) DEFAULT NULL,
+  `trans_abstract` varchar(511) DEFAULT NULL,
+  `trans_title` varchar(255) DEFAULT NULL,
+  `like_count` int NOT NULL DEFAULT '0',
+  `label` varchar(255) DEFAULT 'article',
+  `view_count` int NOT NULL DEFAULT '0',
+  `vtt_id` varchar(255) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=167 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table if not exists video_task
-(
-    id          int          not null
-        primary key,
-    resource_id varchar(255) null,
-    output      varchar(255) null,
-    status      int          null,
-    create_at   timestamp    null,
-    update_at   timestamp    null,
-    check (`status` in (-(1), 0, 1))
-);
+--
+-- Table structure for table `article_like`
+--
 
+DROP TABLE IF EXISTS `article_like`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article_like` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `article_id` int NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `article_like_pk` (`article_id`,`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=290 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `article_view`
+--
+
+DROP TABLE IF EXISTS `article_view`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article_view` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ip` varbinary(30) NOT NULL,
+  `user_id` int NOT NULL DEFAULT '0',
+  `article_id` int NOT NULL,
+  `created_at` datetime NOT NULL,
+  `index` varchar(255) DEFAULT NULL,
+  `platform` varchar(20) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `article_view_pk` (`index`)
+) ENGINE=InnoDB AUTO_INCREMENT=989 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `file` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `create_at` datetime DEFAULT NULL COMMENT 'Create Time',
+  `update_at` datetime DEFAULT NULL COMMENT 'Update Time',
+  `file_key` varchar(255) DEFAULT NULL,
+  `format` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `size` bigint DEFAULT NULL,
+  `duration` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=561 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `share`
+--
+
+DROP TABLE IF EXISTS `share`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `share` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `platform` varchar(20) NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `index_key` varchar(100) DEFAULT NULL,
+  `create_at` datetime DEFAULT NULL,
+  `article_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `share_pk` (`index_key`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `video_subtitle`
+--
+
+DROP TABLE IF EXISTS `video_subtitle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `video_subtitle` (
+  `video_id` int NOT NULL,
+  `subtitle_id` int DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `language` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `video_task`
+--
+
+DROP TABLE IF EXISTS `video_task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `video_task` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `resource_id` varchar(255) DEFAULT NULL,
+  `output` varchar(255) DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `create_at` timestamp NULL DEFAULT NULL,
+  `update_at` timestamp NULL DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `task_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `video_task_chk_1` CHECK ((`status` in (-(1),0,1)))
+) ENGINE=InnoDB AUTO_INCREMENT=211 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-14 14:13:52
